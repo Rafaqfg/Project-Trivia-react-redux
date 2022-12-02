7226384bd78c30dac7f4b4108e4f04a1c4598928
Rafaqfg <rafa.qfg@gmail.com>
2022-12-02 07:03:37 -0300

    feat: :sparkles: implemented home button at the settings page

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Settings extends React.Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <h1 data-testid="settings-title">Configuração</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          className="btn-result"
+          onClick={ () => history.push('/') }
+        >
+          Home
+        </button>
       </div>
     );
   }
 }
+
+Settings.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Settings;
